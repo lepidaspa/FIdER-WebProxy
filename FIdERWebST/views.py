920825f3441604c979c5359062f63784ef07d86f
Antonio Vaccarino <silverdrake@gmail.com>
2012-07-27 17:26:39 +0200

    - Skeleton for StandaloneTool v2

---
+++ FIdERWebST/views.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2012 Laboratori Guglielmo Marconi S.p.A. <http://www.labs.it>
+
+__author__ = 'Antonio Vaccarino'
+__docformat__ = 'restructuredtext en'
+
+
+def uiview (request, **kwargs):
+	"""
+	Loads the interface of the standalone tool
+	:param request:
+	:param kwargs:
+	:return:
+	"""
+
+
+
+
+
