407b7c44a231ccffe42d0764b90a0852d8301953
Antonio Vaccarino <silverdrake@gmail.com>
2012-09-13 17:33:55 +0200

    - Fixed misguided exception when loading WFS data for the first time

--- FIdERProxyFS/proxy_core.py
+++ FIdERProxyFS/proxy_core.py
@@ -146,7 +146,7 @@ def handleDelete (proxy_id, meta_id, shape_id):
 	path_mirror = os.path.join(conf.baseproxypath, proxy_id, conf.path_mirror, meta_id, shape_id)
 
 	if not os.path.exists(path_mirror):
-		raise Exception ("Data for %s/%s already deleted in the mirror section of proxy %s" % (meta_id, shape_id, proxy_id))
+		print "WARNING: Data for %s/%s already deleted in the mirror section of proxy %s" % (meta_id, shape_id, proxy_id)
 	else:
 		#TODO: add specific handling of further exceptions or just push it up the ladder
 		shutil.rmtree(path_mirror)
