1b33d7d3d5f8820e8bd67117f497df54aada0f3f
Antonio Vaccarino <silverdrake@gmail.com>
2012-12-05 11:43:10 +0100

    - Modified construction of feature ids from proxy query

--- FIdERProxyFS/proxy_query.py
+++ FIdERProxyFS/proxy_query.py
@@ -275,7 +275,7 @@ def makeSelectFromJson (proxy_id, meta_id, map_id, jsonmessage):
 			data['type'] = "Feature"
 			data['geometry'] = json.loads(row[fields.index('geometry')])
 
-			data['IDPiper'] = hashlib.md5(json.dumps(data['geometry'])).hexdigest()
+			data['IDPiper'] = 'query_'+proxy_id+hashlib.md5(json.dumps(data['geometry'])).hexdigest()
 
 
 			properties = {}
