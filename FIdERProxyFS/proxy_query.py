15321ea154bc09c3844ec283c6e97708cd404610
Antonio Vaccarino <silverdrake@gmail.com>
2012-12-17 12:09:22 +0100

    - cut idpiper size to 8 chars despite collision weakness

--- FIdERProxyFS/proxy_query.py
+++ FIdERProxyFS/proxy_query.py
@@ -295,10 +295,9 @@ def makeSelectFromJson (proxy_id, meta_id, map_id, jsonmessage):
 
 			data['properties'] = properties
 			piperhash = hashlib.new('md5')
-			#piperhash.digest_size = 8
 			piperhash.update(json.dumps(data['geometry']))
 			#data['properties']['IDPiper'] = 'query_'+proxy_id+hashlib.md5(json.dumps(data['geometry'])).hexdigest()
-			data['properties']['IDPiper'] = 'query_'+proxy_id+piperhash.hexdigest()
+			data['properties']['IDPiper'] = 'query_'+proxy_id+piperhash.hexdigest()[:8]
 
 			try:
 				for unmapped in valuestable['unmapped']:
